d88fa57f3105677172b6f8a6c21b1e6097f1aeba
Isadora <isah3027@gmail.com>
2023-12-01 18:08:44 -0300

    Checkmate logic

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -16,7 +16,7 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while(true){
+        while(!chessMatch.getCheckMate()){
             try{
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -42,7 +42,8 @@ public class Program {
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
-
         }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
